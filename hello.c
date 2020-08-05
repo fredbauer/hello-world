16a61a3238bb297a5385bd9ce3654fada98b7a5b
Fred Bauer <frebauer@cisco.com>
2020-08-04 19:50:43 -0700

    added metrics to display later

--- hello.c
+++ hello.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-int main() {
-   
-   printf("\nHello, World!\n");
-   return 0;
+int
+main ()
+{
+  int i, n;
+  time_t t;
+
+  /* Intializes random number generator */
+  srand ((unsigned) time (&t));
+
+  /* Print 5 random numbers from 0 to 49 */
+  n = rand() % 50;
+  sleep(n);
+
+  printf ("\nHello, World!\n");
+  printf ("execution time: %d\n", n);
+
+  return 0;
 }
